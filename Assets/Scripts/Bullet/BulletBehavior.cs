5726d0f45c126e6c994edd6b31c3050ce47994d4
DerrekNguyen <mnguye21@kent.edu>
2025-09-05 23:01:12 -0400

    Bear with gun

--- Assets/Scripts/Bullet/BulletBehavior.cs
+++ Assets/Scripts/Bullet/BulletBehavior.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BulletBehavior : MonoBehaviour
@@ -7,6 +8,7 @@ public class BulletBehavior : MonoBehaviour
     [HideInInspector] public BulletConfig config;
 
     private Rigidbody2D rb;
+    private BoxCollider2D bc;
 
     void Awake()
     {
@@ -35,6 +37,22 @@ public class BulletBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    // Use FixedUpdate for physics updates
+    void FixedUpdate()
+    {
+        // Move the bullet forward based on its speed
+        rb.velocity = -transform.up * config.speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Wall"))
+        {
+            // Destroy the bullet if it hits a wall
+            Destroy(this.gameObject);
+        }
     }
 }
--- Assets/Scripts/Player/PlayerShooter.cs
+++ Assets/Scripts/Player/PlayerShooter.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerShooter : MonoBehaviour
 {
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+    [Header("Available Bullets")]
+    public List<BulletConfig> bulletTypes;
+
+    private int currentBulletIndex = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        bulletTypes = new List<BulletConfig>(Resources.LoadAll<BulletConfig>("Configs/Bullet"));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Pew Pew");
+            Shoot();
+        }
+    }
+
+    // Method to shoot a bullet
+    void Shoot() {
+        GameObject bulletObj = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        BulletBehavior bullet = bulletObj.GetComponent<BulletBehavior>();
+        bullet.Init(bulletTypes[currentBulletIndex]);
     }
 }
